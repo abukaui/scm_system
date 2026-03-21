8aed2be1a90981eda460c60fb17227401ca91c25
abukaui <abuy3832@gmail.com>
2026-03-21 11:17:02 -0700

    edit config for deployment

--- SCM_System/server/src/db/config.ts
+++ SCM_System/server/src/db/config.ts
@@ -3,13 +3,15 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "Abuka@8539",
-  host: process.env.DB_HOST || "localhost",
-  port: parseInt(process.env.DB_PORT || "5432"),
-  database: process.env.DB_NAME || "student_system"
-});
+export const pool = process.env.DATABASE_URL 
+  ? new Pool({ connectionString: process.env.DATABASE_URL })
+  : new Pool({
+      user: process.env.DB_USER || "postgres",
+      password: process.env.DB_PASSWORD || "Abuka@8539",
+      host: process.env.DB_HOST || "localhost",
+      port: parseInt(process.env.DB_PORT || "5432"),
+      database: process.env.DB_NAME || "student_system"
+    });
 
 pool.on('connect', () => {
     console.log('Successfully connected to the database');
